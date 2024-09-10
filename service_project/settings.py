192093257d897ab045541fe80e398fec31c5697d
VKalaitanov <kalaytanov93@gmail.com>
2024-09-10 12:51:48 +0300

    change settings file

--- service_project/settings.py
+++ service_project/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
-environ.Env.read_env(env_file=Path('./.env.prod'))
+environ.Env.read_env(env_file=Path('./.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
