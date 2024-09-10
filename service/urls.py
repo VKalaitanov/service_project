e7ccfaa22c95cf35e33d5a00ec530362bedb48b4
VKalaitanov <kalaytanov93@gmail.com>
2024-09-10 11:28:04 +0300

    add .env file

--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from . import views
+from django.urls import path
 
 urlpatterns = [
-    path('', home.views),
-
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- service/views.py
+++ service/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'service/index.html')
+
+
+def about(request):
+    return render(
+        request,
+        'service/about.html',
+    )
