da337a5b91e4f4547bc606b0cd46a787ab350f27
VKalaitanov <kalaytanov93@gmail.com>
2024-09-28 17:37:26 +0300

    add static settings

--- service_project/settings.py
+++ service_project/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'staticfiles',
 ]
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
